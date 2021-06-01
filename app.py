ce3536ec6de0304af5c605d523dd172cb5cc51a1
Kipras <kipraszapas@gmail.com>
2021-06-01 13:04:05 -0400

    Updated for Heroku

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from flask import Flask, request, jsonify
 from rq import Queue
 from worker import conn
 from utils import scrapeMap
+from datetime import datetime
 
 app = Flask(__name__)
 q = Queue(connection=conn)
@@ -20,17 +21,25 @@ def get_status(job):
 def handle_job():
     query_id = request.args.get('job')
     query_name = request.args.get('name')
+    query_key = request.args.get('key')
     if query_id:
         found_job = q.fetch_job(query_id)
         if found_job:
             output = get_status(found_job)
         else:
             output = { 'id': None, 'error_message': 'No job exists with the id number ' + query_id }
-    elif query_name:
-        new_job = q.enqueue(scrapeMap, query_name)
-        output = get_status(new_job)
-    else:
+    elif query_name and query_key:
+        if (str(base64.b64decode(api_key))[2:-1] == str(datetime.today()).split()[0]):
+            new_job = q.enqueue(scrapeMap, query_name)
+            output = get_status(new_job)
+        else:
+            output = {'error_message': 'Failed to start due to an invalid API key'}
+    elif query_name == null and query_key:
         output = {'error_message': 'No job can start without a map name'}
+    elif query_key == null and query_name:
+        output = {'error_message': 'No job can start without a valid API key'}
+    else:
+        output = {'error_message': 'No job can start without a map name and a valid API key'}
     return jsonify(output)
 
 if __name__ == "__main__":
--- utils.py
+++ utils.py
@@ -7,7 +7,6 @@ import requests
 import json
 
 def scrapeMap(name):
-
     map_name = name
 
     res = requests.get('https://api.krunker.io/search?type=map&val=' + map_name)
@@ -21,13 +20,13 @@ def scrapeMap(name):
         return "No map found with that name"
 
     options = uc.ChromeOptions()
-    #options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
+    options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
 
     options.add_argument("--headless")
-    #options.add_argument("--disable-dev-shm-usage")
-    #options.add_argument("--no-sandbox")
+    options.add_argument("--disable-dev-shm-usage")
+    options.add_argument("--no-sandbox")
 
-    driver = uc.Chrome(options=options, enable_cdp_events=True)
+    driver = uc.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), options=options, enable_cdp_events=True)
 
     async def main():
         def printResponse(eventdata):
@@ -35,17 +34,41 @@ def scrapeMap(name):
             data = base64.b64decode(payload)
             size = (len(data) * 3) / 4
             if (size > 4000):
-                print(data)
+                print("Loaded large websocket packet")
+                try:
+                    start = str(data).find('{')
+                except:
+                    print("error parsing data (start)")
+                    driver.quit()
+                    return "error parsing data (start)"
+                try:
+                    end = str(data).rfind(']}') + 2
+                except:
+                    print("error parsing data (end)")
+                    driver.quit()
+                    return "error parsing data (end)"
+                print("end of file: {}".format(end))
+                codeu = str(data)[start:end]
+                try:
+                    code = codeu.replace(""","col":false""", "")
+                except:
+                    print("no occurences of 'col:false;'")
+                print("Successfully loaded map code for {}".format(map_name))
                 driver.quit()
-                return data
+                return code
 
         driver.get('https://krunker.io')
 
         print("website loaded")
+        sleep(2)
 
-        driver.execute_script("selectHostMap('{0}','undefined','{1}','{2}',1)".format(map_name, map_id, map_creator))
-        print("host function executed")
-        sleep(0.6)
+        try:
+            driver.execute_script("selectHostMap('{0}','undefined','{1}','{2}',1)".format(map_name, map_id, map_creator))
+            print("host function executed")
+            sleep(1)
+        except:
+            driver.quit()
+            return "failed operation at host map"
 
         driver.execute_script("createPrivateRoom()")
         print("started hoster")

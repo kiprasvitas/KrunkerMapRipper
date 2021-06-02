dacbe4cadd0d8e827653a1a1a2526f9d1be062c6
Kipras <kipraszapas@gmail.com>
2021-06-02 16:53:03 -0400

    Reworked entire script
    Remove asyncio
    Return results of the map data
    Unreliable as hell
    Welp I guess that's as good as its gonna get.

--- utils.py
+++ utils.py
@@ -5,8 +5,10 @@ import os
 import asyncio
 import requests
 import json
+import sys
 
-websocket_count = 0;
+websocket_count = 0
+returnvalue = "Couldn't load map data. Try again."
 
 def scrapeMap(name):
     map_name = name
@@ -18,50 +20,40 @@ def scrapeMap(name):
     options.add_argument("--disable-dev-shm-usage")
     options.add_argument("--no-sandbox")
 
-    driver = uc.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), options=options, enable_cdp_events=True)
+    driver = uc.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), keep_alive=True, version_main=90, patcher_force_close=True, options=options, enable_cdp_events=True)
 
-    async def main():
-        def printResponse(eventdata):
-            global websocket_count
-            payload = eventdata['params']['response']['payloadData']
-            data = base64.b64decode(payload)
-            size = (len(data) * 3) / 4
-            if (size > 4000):
-                print("Loaded large websocket packet")
-                try:
-                    start = str(data).find('{')
-                except:
-                    print("error parsing data (start)")
-                    driver.quit()
-                    return "error parsing data (start)"
-                try:
-                    end = str(data).rfind(']}') + 2
-                except:
-                    print("error parsing data (end)")
-                    driver.quit()
-                    return "error parsing data (end)"
-                print("end of file: {}".format(end))
-                codeu = str(data)[start:end]
-                try:
-                    code = codeu.replace(""","col":false""", "")
-                except:
-                    print("no occurences of 'col:false;'")
-                print("Successfully loaded map code for {}".format(map_name))
-                driver.quit()
-                return code
-            elif (websocket_count < 10):
-                websocket_count = websocket_count + 1
-            else:
+    def printResponse(eventdata):
+        global websocket_count
+        global returnvalue
+        payload = eventdata['params']['response']['payloadData']
+        data = base64.b64decode(payload)
+        size = (len(data) * 3) / 4
+        if (size > 4000 and returnvalue == "Couldn't load map data. Try again."):
+            print("Loaded large websocket packet")
+            start = str(data).find('{')
+            end = str(data).rfind(']}') + 2
+            print("end of file: {}".format(end))
+            codeu = str(data)[start:end]
+            try:
+                code = codeu.replace(""","col":false""", "")
+            except:
+                print("no occurences of 'col:false;'")
+            print("Successfully loaded map code for {}".format(map_name))
+            returnvalue = code
+
+        elif (websocket_count < 40 and returnvalue == "Couldn't load map data. Try again."):
+            websocket_count = websocket_count + 1
+        else:
+            if (returnvalue == "Couldn't load map data. Try again."):
                 print("No map data to return")
-                driver.quit()
-                return "No map data was found at given map name."
+                returnvalue = "No map data was found at given map name."
 
+    with driver:
+        driver.add_cdp_listener("Network.webSocketFrameReceived", printResponse)
+        driver.add_cdp_listener("Network.webSocketFrameSent", printResponse)
         driver.get('https://krunker.io/?play={}'.format(map_name))
         print("website loaded")
 
-        driver.add_cdp_listener("Network.webSocketFrameReceived", printResponse)
-        driver.add_cdp_listener("Network.webSocketFrameSent", printResponse)
-
-        await asyncio.sleep(2000)
-
-    asyncio.get_event_loop().run_until_complete(main())
+    sleep(10)
+    driver.quit()
+    return returnvalue
